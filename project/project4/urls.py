d9bafad9f042ef88524776e812f8226307d57d69
ghosthubg <91081294+ghosthubg@users.noreply.github.com>
2023-11-22 15:28:55 +0200

    initial commit

--- project/project4/urls.py
+++ project/project4/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("", include("network.urls")),
 ]
 
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
